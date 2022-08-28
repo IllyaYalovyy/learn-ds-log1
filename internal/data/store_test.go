b0db247f5e35e1b6dc85f62c42c4461147cef577
Illya Yalovyy <yalovoy@gmail.com>
2022-08-28 14:43:22 -0700

    added unit tests for ReadAt and Close

--- internal/data/store_test.go
+++ internal/data/store_test.go
@@ -14,17 +14,45 @@ var (
 )
 
 func TestStore_AppendRead(t *testing.T) {
+	s := testCreateStore(t)
+	testAppendData(t, s)
+	testRead(t, s)
+}
+
+func TestStore_ReadAt(t *testing.T) {
 	require := require.New(t)
 	assert := assert.New(t)
+
+	s := testCreateStore(t)
+	testAppendData(t, s)
+
+	message := testMessages[0]
+	data := make([]byte, len(message))
+	numBytes, err := s.ReadAt(data, recordLenSize)
+	require.NoError(err)
+	assert.Equal(message, string(data), "written data")
+	assert.Equal(len(message), numBytes)
+}
+
+func testCreateStore(t *testing.T) *Store {
+	t.Helper()
+	require := require.New(t)
+
 	testDir := t.TempDir()
 	f, err := os.Create(filepath.Join(testDir, "log_store.bin"))
 	require.NoError(err)
 	s, err := NewStore(f)
 	require.NoError(err)
+	return s
+}
+
+func testAppendData(t *testing.T, s *Store) {
+	t.Helper()
+	require := require.New(t)
+	assert := assert.New(t)
 
 	var expectedPos int64
 	for _, message := range testMessages {
-
 		n, pos, err := s.Append([]byte(message))
 		require.NoError(err)
 		assert.Equal(expectedPos, pos, "record position")
@@ -32,6 +60,12 @@ func TestStore_AppendRead(t *testing.T) {
 		assert.Equal(expectedRecordSize, n, "record size")
 		expectedPos += expectedRecordSize
 	}
+}
+
+func testRead(t *testing.T, s *Store) {
+	t.Helper()
+	require := require.New(t)
+	assert := assert.New(t)
 
 	var readPos int64
 	for _, message := range testMessages {
@@ -40,4 +74,22 @@ func TestStore_AppendRead(t *testing.T) {
 		assert.Equal(message, string(data), "written data")
 		readPos += int64(len(data) + recordLenSize)
 	}
+}
+
+func TestStore_CloseOpenStore(t *testing.T) {
+	require := require.New(t)
+	s := testCreateStore(t)
+	testAppendData(t, s)
+
+	fileName := s.Name()
+
+	err := s.Close()
+	require.NoError(err)
+
+	f, err := os.Open(fileName)
+	require.NoError(err)
+
+	s, err = NewStore(f)
+	require.NoError(err)
+	testRead(t, s)
 }
